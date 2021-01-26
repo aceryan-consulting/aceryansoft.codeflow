2331288986f2d2a6ceef0b349f3c588bbb2d18da
Yannick Nganko <cyrille.nganko@aceryan-consulting.fr>
2021-01-26 16:19:01 +0100

    add extension method code sample

---
+++ src/aceryan.codeflow.samples/GettingStarted/ExtensionCodeFlow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using aceryansoft.codeflow.core;
+using aceryansoft.codeflow.model.Config;
+using aceryansoft.codeflow.model.FlowApi;
+
+
+namespace aceryan.codeflow.samples.GettingStarted
+{ 
+    public class ExtensionCodeFlow
+    {
+        public void SampleCode()
+        {
+            var hello = new CodeFlow();
+            hello.StartNew()
+                .Call((ctx, inputs) =>
+                {
+                    Console.WriteLine("Hello world");
+                })
+                .CustomExtension((ctx, inputs) => // add CustomExtension to the fluent api
+                {
+                    Console.WriteLine("How are you today ?");
+                })
+                .Close();
+            hello.Execute();
+        }
+    }
+
+
+    public static class MyExtensions
+    {
+        public static ICodeFlowApi CustomExtension(this ICodeFlowApi codeFlowApi,Action<ICodeFlowContext, object[]> customAction)
+        {
+            codeFlowApi.Call((ctx, inputs) =>   
+            {
+                // write awesome logic before  
+                customAction(ctx,inputs);
+                // write awesome logic after  
+            });
+            return codeFlowApi; 
+        }
+    }
+
+
+}
